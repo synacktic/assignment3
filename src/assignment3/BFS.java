97e85e36e491096b72855d5cfc64d7063a004cb4
synacktic <s.github@zeniquity.com>
2016-09-27 13:17:27 -0500

    Syncing up

--- src/assignment3/BFS.java
+++ src/assignment3/BFS.java
@@ -2,6 +2,7 @@ package assignment3;
 
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.PriorityQueue;
 
 public class BFS {
 	//final static Set<String> dict = Main.makeDictionary();
@@ -10,27 +11,44 @@ public class BFS {
 	public static ArrayList<String> findLadder(String start, String end, Set<String> dict){
 		String[] adict;
 		adict = dict.toArray(new String[0]);
-		System.out.printf("%s\n",adict[0]);
-		int dictCount = 0;
-		while (dictCount < adict.length) {
-			if ( Main.letterDifference(start,adict[dictCount]))  {
-				System.out.printf("%s and %s are just one letter away!\n",start,adict[dictCount]);
-			}
-			
-/*
-			int letterCount = 0;
-			int badletters = 0;
-			while (letterCount < adict[dictCount].length()) {
-				if () { }
-				letterCount++;
-			}*/
-			dictCount++;
-		}
-		if (dict.contains(start)) {
+		//System.out.printf("%s\n",adict[0]);
+	       //Comparator<String> comparator = new StringLengthComparator();
+	        PriorityQueue<String> queue = new PriorityQueue<String>(1000);
+	        queue.add(start);
+	   		int found = 0;
+
+	       while (queue.size() != 0 && found == 0)
+	       {
+	            String path = queue.remove();
+	            String[] parts = path.split(",");
+	            String toComp = parts[parts.length-1];
+		   		int dictCount = 0;
+				while (dictCount < adict.length && found == 0) {
+					if ( Main.letterDifference(toComp,adict[dictCount]))  {
+						queue.add(path + "," + adict[dictCount]);
+						//System.out.printf("%s and %s are just one letter away!\n",toComp,adict[dictCount]);
+				
+						if ( Main.letterDifference(end,adict[dictCount])) {
+							System.out.printf("%s and %s are just one letter away!\n",end,adict[dictCount]);
+							System.out.printf("Ladder has been found:\n %s",path + "," + adict[dictCount] + "," + end);
+							found++;
+						}
+						adict[dictCount] = "00000"; // FIVE
+					}
+					dictCount++;
+				
+				}
+	       }
+	     //  while (queue.size() != 0) {
+	    	//   System.out.println(queue.remove());
+	      // }
+ 
+	
+	/*	if (dict.contains(start)) {
 			System.out.println("Hey! I know that word!");
 		} else {
 			System.out.println("What word is this?!!?");
-		}
+		}*/
 		
 		return null;
 	}
